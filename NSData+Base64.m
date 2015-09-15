12b9407297e3ef0f9a51d1d61bf822300b73f480
Malte Tancred <malte@tancred.com>
2015-09-15 13:37:50 +0200

    NSData can decode complete blocks.

--- NSData+Base64.m
+++ NSData+Base64.m
@@ -1,10 +1,41 @@
 #import "NSData+Base64.h"
 #import "GenericBase64Decoder.h"
 
+
+static int base64value(char c);
+
+
 @implementation NSData (Base64)
 
 - (NSData *)dataByDecodingBase64:(NSError **)error {
-	return [NSData data];
+	NSUInteger length = [self length];
+	if (length == 0) return [NSData data];
+
+	__block NSMutableData *decoded = [NSMutableData new];
+	GenericBase64Decoder *decoder = [GenericBase64Decoder new];
+	decoder.output = ^(char *b, NSUInteger len) {
+		[decoded appendBytes:b length:len];
+	};
+
+	const char *encoded = [self bytes];
+	for (NSUInteger i=0; i<length; i++) {
+		[decoder input:base64value(encoded[i])];
+		if (decoder.state == GenericBase64DecoderStateIllegal) break;
+	}
+
+	[decoder input:GenericBase64DecoderTokenEOF];
+
+	return decoded;
 }
 
 @end
+
+
+static int base64value(char c) {
+	if (c >= 'A' && c <= 'Z') return c - 'A';
+	if (c >= 'a' && c <= 'z') return c - 'a' + 26;
+	if (c >= '0' && c <= '9') return c - '0' + 52;
+	if (c == '+') return 62;
+	if (c == '/') return 63;
+	return GenericBase64DecoderTokenOther;
+}
--- NSDataDecoderTest.m
+++ NSDataDecoderTest.m
@@ -7,10 +7,33 @@
 @end
 
 
+#define STR2DATA(str) [NSData dataWithBytes:(str) length:strlen((str))]
+#define STR2DATAl(str,len) [NSData dataWithBytes:(str) length:(len)]
+
+
 @implementation NSDataDecoderTest
 
 - (void)testEmpty {
 	XCTAssertEqualObjects([[NSData data] dataByDecodingBase64:NULL], [NSData data]);
+}
+
+- (void)testDecodeBlock {
+	NSError *error = nil;
+	XCTAssertEqualObjects([STR2DATA("AAAA") dataByDecodingBase64:&error], STR2DATAl("\0\0\0", 3), @"error: %@", error);
+	XCTAssertEqualObjects([STR2DATA("AAAB") dataByDecodingBase64:&error], STR2DATAl("\0\0\x01", 3), @"error: %@", error);
+	XCTAssertEqualObjects([STR2DATA("AAAZ") dataByDecodingBase64:&error], STR2DATAl("\0\0\x19", 3), @"error: %@", error);
+	XCTAssertEqualObjects([STR2DATA("AAAa") dataByDecodingBase64:&error], STR2DATAl("\0\0\x1a", 3), @"error: %@", error);
+	XCTAssertEqualObjects([STR2DATA("AAAz") dataByDecodingBase64:&error], STR2DATAl("\0\0\x33", 3), @"error: %@", error);
+	XCTAssertEqualObjects([STR2DATA("AAA0") dataByDecodingBase64:&error], STR2DATAl("\0\0\x34", 3), @"error: %@", error);
+	XCTAssertEqualObjects([STR2DATA("AAA9") dataByDecodingBase64:&error], STR2DATAl("\0\0\x3d", 3), @"error: %@", error);
+	XCTAssertEqualObjects([STR2DATA("AAA+") dataByDecodingBase64:&error], STR2DATAl("\0\0\x3e", 3), @"error: %@", error);
+	XCTAssertEqualObjects([STR2DATA("AAA/") dataByDecodingBase64:&error], STR2DATAl("\0\0\x3f", 3), @"error: %@", error);
+
+	XCTAssertEqualObjects([STR2DATA("Aa0+") dataByDecodingBase64:&error], STR2DATAl("\x01\xad\x3e", 3), @"error: %@", error);
+	XCTAssertEqualObjects([STR2DATA("Yy9/") dataByDecodingBase64:&error], STR2DATAl("\x63\x2f\x7f", 3), @"error: %@", error);
+	XCTAssertEqualObjects([STR2DATA("PQfg") dataByDecodingBase64:&error], STR2DATAl("\x3d\x07\xe0", 3), @"error: %@", error);
+	XCTAssertEqualObjects([STR2DATA("IPf+") dataByDecodingBase64:&error], STR2DATAl("\x20\xf7\xfe", 3), @"error: %@", error);
+	XCTAssertEqualObjects([STR2DATA("BQg/") dataByDecodingBase64:&error], STR2DATAl("\x05\x08\x3f", 3), @"error: %@", error);
 }
 
 @end
