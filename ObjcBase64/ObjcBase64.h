fdc9bc2009b28006fcb20514c53f66d929e353ce
Malte Tancred <malte@tancred.com>
2015-10-27 09:36:05 +0100

    Add Xcode project scaffold.

---
+++ ObjcBase64/ObjcBase64.h
@@ -0,0 +1,19 @@
+//
+//  ObjcBase64.h
+//  ObjcBase64
+//
+//  Created by Malte Tancred on 2015-10-27.
+//  Copyright © 2015 Tancred. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ObjcBase64.
+FOUNDATION_EXPORT double ObjcBase64VersionNumber;
+
+//! Project version string for ObjcBase64.
+FOUNDATION_EXPORT const unsigned char ObjcBase64VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjcBase64/PublicHeader.h>
+
+
